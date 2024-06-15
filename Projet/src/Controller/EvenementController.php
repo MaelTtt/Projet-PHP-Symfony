948c10cb442758e0e74bc0de54302805c95d5e55
DupuySamson <samson.dupuy@edu.univ-fcomte.fr>
2024-06-15 13:10:51 +0200

    add doctrine + evenement creation

---
+++ Projet/src/Controller/EvenementController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Evenement;
+use App\Form\EvenementType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EvenementController extends AbstractController
+{
+    /**
+     * @Route("/evenement/new", name="evenement_new")
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $evenement = new Evenement();
+        $form = $this->createForm(EvenementType::class, $evenement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($evenement);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('evenement_list');
+        }
+
+        return $this->render('evenement/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
+
