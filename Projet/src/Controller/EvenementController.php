b36c544176936673d0956101ad6bb94e9e1103c1
DupuySamson <samson.dupuy@edu.univ-fcomte.fr>
2024-06-15 14:14:33 +0200

    affichage partiel des evenements après création

--- Projet/src/Controller/EvenementController.php
+++ Projet/src/Controller/EvenementController.php
@@ -9,6 +9,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use App\Repository\EvenementRepository;
+use Knp\Component\Pager\PaginatorInterface;
+
 class EvenementController extends AbstractController
 {
     /**
@@ -30,6 +33,41 @@ class EvenementController extends AbstractController
 
         return $this->render('evenement/new.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/evenement", name="evenement_list")
+     */
+    //AuthorizationCheckerInterface $authChecker
+    public function list(EvenementRepository $evenementRepository, PaginatorInterface $paginator, Request $request)
+    {
+        $queryBuilder = $evenementRepository->createQueryBuilder('e');
+
+        //if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+        //    $queryBuilder->andWhere('e.isPublic = true');
+        //}
+
+        $pagination = $paginator->paginate(
+            $queryBuilder->getQuery(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('evenement/list.html.twig', [
+            'pagination' => $pagination,
+        ]);
+    }
+
+    /**
+     * @Route("/evenement/{id}", name="evenement_show")
+     */
+    public function show(Evenement $evenement)
+    {
+        $this->denyAccessUnlessGranted('view', $evenement);
+
+        return $this->render('event/show.html.twig', [
+            'event' => $evenement,
         ]);
     }
 }
